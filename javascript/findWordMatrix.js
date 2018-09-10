047026e500af0095dc64df105f544359f55ce6b6
Angel Cazares <angel.cazares2@gmail.com>
2018-09-09 22:18:31 -0500

    Update findWordMatrix.js

--- javascript/findWordMatrix.js
+++ javascript/findWordMatrix.js
@@ -10,8 +10,8 @@
 *
 *    word: solar
 *    return: true
-*    The word can be created because the 's' char is connected with the 'o' char, that also is
-*    connected with 'l'
+*    The word can be created because the 's' char is connected with the 'o' char, which is also
+*    connected with 'l'...
 */
 
 (function() {
